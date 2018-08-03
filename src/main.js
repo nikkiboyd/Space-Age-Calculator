d2a047e01b3dce5da49af0e3c4897588b40f5b8e
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-03 13:39:33 -0700

    grab form values using jquery

--- src/main.js
+++ src/main.js
@@ -7,5 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
   $("space-age").submit(function(event) {
     event.preventDefault();
+    let birthMonth = $("#birth-month").val();
+    let birthDate = $("#birth-date").val();
+    let birthYear = $("#birth-year").val();
+    let birthDay = new Age(birthYear, birthMonth, birthDate);
   });
 });
